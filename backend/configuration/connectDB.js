d50f178684e22dd4b0e2536b06e3fe480e409434
Riya Rajput <riyarrrajput053@gmail.com>
2025-03-09 22:45:09 +0530

    updated database url

--- backend/configuration/connectDB.js
+++ backend/configuration/connectDB.js
@@ -2,7 +2,7 @@ const mongoose = require("mongoose")
 
 async function DataBase() {
     try {
-        await mongoose.connect("mongodb://127.0.0.1:27017/reportManager")
+        await mongoose.connect("mongodb+srv://riyarrrajput053:<db_password>@cluster0.x9ecs.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0")
         .then((value) => {
             console.log("Database connected");
         })
